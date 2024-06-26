68445a41ea2416195c4fd153c72ecf5a51912a90
adrianvillafan <106986651+adrianvillafan@users.noreply.github.com>
2024-06-26 11:36:11 -0500

    cambio de url
    Vamos sport boys

--- app.py
+++ app.py
@@ -11,52 +11,51 @@ def obtener_datos_dni():
         return jsonify({"detail": "El DNI debe tener 8 dígitos"}), 400
 
     try:
-        # Obtención de cookies
+        # Obtención de cookies y del token
         s = requests.Session()
-        res1 = s.get("https://dni-peru.com/buscar-dni-nombres-apellidos/")
+        res1 = s.get("https://eldni.com/pe/buscar-por-dni")
         if res1.status_code != 200:
             raise Exception(f"Error al obtener cookies: {res1.status_code}")
         
+        # Parsear el contenido HTML para obtener el token
+        soup = BeautifulSoup(res1.text, 'html.parser')
+        token = soup.find('input', {'name': '_token'}).get('value')
+        if not token:
+            raise Exception("No se encontró el token en el HTML")
+
         # Envío de información
-        res2 = s.post("https://dni-peru.com/buscar-dni-nombres-apellidos/", data={'dni4': dni, 'buscar_dni': ''})
+        res2 = s.post("https://eldni.com/pe/buscar-datos-por-dni", 
+                      files={
+                          '_token': (None, token),
+                          'dni': (None, dni)
+                      })
         if res2.status_code != 200:
             raise Exception(f"Error al enviar datos: {res2.status_code}")
 
-        # Obtención de variables
+        # Parsear el HTML de la respuesta
         soup = BeautifulSoup(res2.text, 'html.parser')
+        table = soup.find('table', {'class': 'table table-striped table-scroll'})
+        if not table:
+            raise Exception("No se encontró la tabla de resultados")
 
-        # Depuración: imprimir el HTML completo recibido
-        html_content = soup.prettify()
-        print(html_content)
+        # Extraer los datos de la tabla
+        rows = table.find_all('tr')
+        if len(rows) < 2:
+            raise Exception("No se encontraron suficientes filas en la tabla de resultados")
 
-        div = soup.find('div', {'id': 'resultado_busqueda'})
-        if not div:
-            raise Exception("No se encontró el div de resultado de búsqueda")
-        
-        datos = div.find_all('p', recursive=True)
-        if len(datos) < 5:
-            raise Exception("No se encontraron suficientes datos en el resultado de búsqueda")
+        cols = rows[1].find_all('td')
+        if len(cols) < 4:
+            raise Exception("No se encontraron suficientes columnas en la fila de resultados")
 
-        try:
-            bs_nombre = BeautifulSoup(str(datos[2]), 'html.parser')
-            p_nombre = bs_nombre.find('p')
-            nombre = ''.join(p_nombre.find_all(string=True, recursive=False)).strip()
+        nombre = cols[1].text.strip()
+        apellido_paterno = cols[2].text.strip()
+        apellido_materno = cols[3].text.strip()
 
-            bs_paterno = BeautifulSoup(str(datos[3]), 'html.parser')
-            p_paterno = bs_paterno.find('p')
-            paterno = ''.join(p_paterno.find_all(string=True, recursive=False)).strip()
-
-            bs_materno = BeautifulSoup(str(datos[4]), 'html.parser')
-            p_materno = bs_materno.find('p')
-            materno = ''.join(p_materno.find_all(string=True, recursive=False)).strip()
-
-            return jsonify({
-                "nombres": nombre.strip(),
-                "apellido_paterno": paterno.strip(),
-                "apellido_materno": materno.strip()
-            })
-        except Exception as e:
-            raise Exception(f"Error procesando los datos: {str(e)}")
+        return jsonify({
+            "nombres": nombre,
+            "apellido_paterno": apellido_paterno,
+            "apellido_materno": apellido_materno
+        })
     except Exception as e:
         return jsonify({"detail": f"Error al obtener los datos: {str(e)}"}), 500
 
